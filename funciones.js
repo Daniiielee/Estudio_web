742c15aeb80b052f9ed468dc72a0de2f41e21d83
Daniiielee <daniestevan26@gmail.com>
2025-08-05 21:26:04 -0500

    v1

---
+++ funciones.js
@@ -0,0 +1,10 @@
+function mensaje(){
+    const h3 = document.getElementById("mensaje");
+    h3.textContent="Te tebuuuu muchoteeeee";
+    const h2 = document.getElementById("titulo2");
+    h2.textContent="Mira abajo :3"
+    const h1 = document.getElementById("titulo1");
+    h1.textContent="<3"
+    const p = document.getElementById("parrafo1");
+    p.textContent=" "
+}
